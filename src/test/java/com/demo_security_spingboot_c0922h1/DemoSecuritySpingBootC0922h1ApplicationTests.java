491892ffe2a284e7fbf89c63729ac42bd21e9bcf
toannvph05233 <johntoan98@gmail.com>
2023-02-07 11:43:46 +0700

    up

---
+++ src/test/java/com/demo_security_spingboot_c0922h1/DemoSecuritySpingBootC0922h1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.demo_security_spingboot_c0922h1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoSecuritySpingBootC0922h1ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
